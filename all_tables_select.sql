8454cf0c22077d781eb82323f9b39e4301711762
otis24680 <otis24680@gmail.com>
2025-04-07 17:47:26 +0200

    2

--- all_tables_select.sql
+++ all_tables_select.sql
@@ -2,6 +2,6 @@ SELECT * FROM Genres;
 SELECT * FROM Directors;
 SELECT * FROM Actors;
 SELECT * FROM Users;
-SELECT * FROM Films;
+SELECT Title FROM Films;
 SELECT * FROM Film_Actors;
 SELECT * FROM Reviews;
--- insert_nondependent.sql
+++ insert_nondependent.sql
@@ -26,14 +26,28 @@ INSERT INTO Actors (Name, Birthdate) VALUES
 ('Emma Stone', '1988-11-06'),
 ('Matthew McConaughey', '1969-11-04'),
 ('Anne Hathaway', '1982-11-12'),
-('Jessica Chastain', '1977-03-24');
---počet záznamů: 7
+('Jessica Chastain', '1977-03-24'),
+('Leonardo DiCaprio', '1974-11-11'),         -- Titanic, The Departed
+('Kate Winslet', '1975-10-05'),              -- Titanic
+('Joe Pesci', '1943-02-09'),                 -- The Irishman, Raging Bull
+('Robert De Niro', '1943-08-17'),            -- The Irishman, Raging Bull
+('Al Pacino', '1940-04-25'),                 -- The Irishman
+('Cillian Murphy', '1976-05-25'),            -- Dunkirk
+('Guy Pearce', '1967-10-05'),                -- Memento
+('Ellen Burstyn', '1932-12-07'),             -- Interstellar
+('Harry Dean Stanton', '1926-07-14'),        -- Alien
+('Sigourney Weaver', '1949-10-08'),          -- Alien
+('Christian Bale', '1974-01-30'),            -- The Prestige
+('Hugh Jackman', '1968-10-12'),              -- The Prestige
+('Jamie Lee Curtis', '1958-11-22'),          -- True Lies
+('Arnold Schwarzenegger', '1947-07-30');     -- True Lies
+--počet záznamů: 21
 
 -- Insert Users
 INSERT INTO Users (Username, Email) VALUES 
 ('FilmBuff2025', 'filmbuff2025@example.com'), 
 ('ReviewerX', 'reviewerx@example.com'), 
-('CineGeek', 'cinegeek@example.com');
+('CineGeek', 'cinegeek@example.com'),
 ('MovieFanatic', 'moviefanatic@example.com'),
 ('ClassicLover', 'classiclover@example.com'),
 ('IndieWatcher', 'indiewatcher@example.com'),
@@ -46,4 +60,4 @@ INSERT INTO Users (Username, Email) VALUES
 ('TheRealCritic', 'therealcritic@example.com');
 --počet záznamů: 13
 
---počet celkových záznamů: 32
+--počet celkových záznamů: 46

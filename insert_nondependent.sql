8ab1cb005faafb3b288e3b1af9af97a9dce1f25e
otis24680 <otis24680@gmail.com>
2025-05-29 11:10:33 +0200

    finální úprava insert_nondependent.sql

--- insert_nondependent.sql
+++ insert_nondependent.sql
@@ -1,4 +1,4 @@
--- Insert Genres
+-- Žánry
 INSERT INTO Genres (Genre_Name) VALUES 
 ('Sci-Fi'), 
 ('Thriller'), 
@@ -10,7 +10,7 @@ INSERT INTO Genres (Genre_Name) VALUES
 ('Fantasy');
 --počet záznamů: 8
 
--- Insert Directors
+-- Režiséři
 INSERT INTO Directors (Name, Birthdate) VALUES 
 ('Steven Spielberg', '1946-12-18'), 
 ('James Cameron', '1954-08-16'), 
@@ -22,7 +22,7 @@ INSERT INTO Directors (Name, Birthdate) VALUES
 ('Woody Allen', '1935-12-01');
 --počet záznamů: 8
 
--- Insert Actors
+-- Herci
 INSERT INTO Actors (Name, Birthdate) VALUES 
 ('Tom Hanks', '1956-07-09'), 
 ('Natalie Portman', '1981-06-09'), 
@@ -31,20 +31,20 @@ INSERT INTO Actors (Name, Birthdate) VALUES
 ('Matthew McConaughey', '1969-11-04'),
 ('Anne Hathaway', '1982-11-12'),
 ('Jessica Chastain', '1977-03-24'),
-('Leonardo DiCaprio', '1974-11-11'),         -- Titanic, The Departed
-('Kate Winslet', '1975-10-05'),              -- Titanic
-('Joe Pesci', '1943-02-09'),                 -- The Irishman, Raging Bull
-('Robert De Niro', '1943-08-17'),            -- The Irishman, Raging Bull
-('Al Pacino', '1940-04-25'),                 -- The Irishman
-('Cillian Murphy', '1976-05-25'),            -- Dunkirk
-('Guy Pearce', '1967-10-05'),                -- Memento
-('Ellen Burstyn', '1932-12-07'),             -- Interstellar
-('Harry Dean Stanton', '1926-07-14'),        -- Alien
-('Sigourney Weaver', '1949-10-08'),          -- Alien
-('Christian Bale', '1974-01-30'),            -- The Prestige
-('Hugh Jackman', '1968-10-12'),              -- The Prestige
-('Jamie Lee Curtis', '1958-11-22'),          -- True Lies
-('Arnold Schwarzenegger', '1947-07-30'),     -- True Lies
+('Leonardo DiCaprio', '1974-11-11'),         
+('Kate Winslet', '1975-10-05'),              
+('Joe Pesci', '1943-02-09'),                 
+('Robert De Niro', '1943-08-17'),            
+('Al Pacino', '1940-04-25'),                 
+('Cillian Murphy', '1976-05-25'),            
+('Guy Pearce', '1967-10-05'),                
+('Ellen Burstyn', '1932-12-07'),             
+('Harry Dean Stanton', '1926-07-14'),        
+('Sigourney Weaver', '1949-10-08'),          
+('Christian Bale', '1974-01-30'),            
+('Hugh Jackman', '1968-10-12'),              
+('Jamie Lee Curtis', '1958-11-22'),          
+('Arnold Schwarzenegger', '1947-07-30'),     
 
 -- Pulp Fiction
 ('John Travolta', '1954-02-18'),

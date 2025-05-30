ff27a15eb8758d34c1f454a9bd82ef2936e19334
otis24680 <otis24680@gmail.com>
2025-05-30 13:18:53 +0200

    finální změny

--- insert_nondependent.sql
+++ insert_nondependent.sql
@@ -10,122 +10,82 @@ INSERT INTO Genres (Genre_Name) VALUES
 ('Fantasy');
 --počet záznamů: 8
 
--- Režiséři
-INSERT INTO Directors (Name, Birthdate) VALUES 
-('Steven Spielberg', '1946-12-18'), 
-('James Cameron', '1954-08-16'), 
-('Martin Scorsese', '1942-11-17'),
-('Christopher Nolan', '1970-07-30'),
-('Quentin Tarantino', '1963-03-27'),
-('Ridley Scott', '1937-11-30'),
-('Clint Eastwood', '1930-05-31'),
-('Woody Allen', '1935-12-01');
---počet záznamů: 8
+-- Insert Directors (rozdělené jméno a příjmení)
+INSERT INTO Directors (First_Name, Last_Name, Birthdate) VALUES
+('Steven', 'Spielberg',    '1946-12-18'),
+('James',  'Cameron',       '1954-08-16'),
+('Martin', 'Scorsese',      '1942-11-17'),
+('Christopher', 'Nolan',    '1970-07-30'),
+('Quentin', 'Tarantino',    '1963-03-27'),
+('Ridley',  'Scott',        '1937-11-30'),
+('Clint',   'Eastwood',     '1930-05-31'),
+('Woody',   'Allen',        '1935-12-01');
 
--- Herci
-INSERT INTO Actors (Name, Birthdate) VALUES 
-('Tom Hanks', '1956-07-09'), 
-('Natalie Portman', '1981-06-09'), 
-('Denzel Washington', '1954-12-28'), 
-('Emma Stone', '1988-11-06'),
-('Matthew McConaughey', '1969-11-04'),
-('Anne Hathaway', '1982-11-12'),
-('Jessica Chastain', '1977-03-24'),
-('Leonardo DiCaprio', '1974-11-11'),         
-('Kate Winslet', '1975-10-05'),              
-('Joe Pesci', '1943-02-09'),                 
-('Robert De Niro', '1943-08-17'),            
-('Al Pacino', '1940-04-25'),                 
-('Cillian Murphy', '1976-05-25'),            
-('Guy Pearce', '1967-10-05'),                
-('Ellen Burstyn', '1932-12-07'),             
-('Harry Dean Stanton', '1926-07-14'),        
-('Sigourney Weaver', '1949-10-08'),          
-('Christian Bale', '1974-01-30'),            
-('Hugh Jackman', '1968-10-12'),              
-('Jamie Lee Curtis', '1958-11-22'),          
-('Arnold Schwarzenegger', '1947-07-30'),     
+-- Insert Actors (rozdělené jméno a příjmení)
+INSERT INTO Actors (First_Name, Last_Name, Birthdate) VALUES
+('Tom',        'Hanks',               '1956-07-09'),
+('Natalie',    'Portman',             '1981-06-09'),
+('Denzel',     'Washington',          '1954-12-28'),
+('Emma',       'Stone',               '1988-11-06'),
+('Matthew',    'McConaughey',         '1969-11-04'),
+('Anne',       'Hathaway',            '1982-11-12'),
+('Jessica',    'Chastain',            '1977-03-24'),
+('Leonardo',   'DiCaprio',            '1974-11-11'),
+('Kate',       'Winslet',             '1975-10-05'),
+('Joe',        'Pesci',               '1943-02-09'),
+('Robert',     'De Niro',             '1943-08-17'),
+('Al',         'Pacino',              '1940-04-25'),
+('Cillian',    'Murphy',              '1976-05-25'),
+('Guy',        'Pearce',              '1967-10-05'),
+('Ellen',      'Burstyn',             '1932-12-07'),
+('Harry',      'Dean Stanton',        '1926-07-14'),
+('Sigourney',  'Weaver',              '1949-10-08'),
+('Christian',  'Bale',                '1974-01-30'),
+('Hugh',       'Jackman',             '1968-10-12'),
+('Jamie Lee',  'Curtis',              '1958-11-22'),
+('Arnold',     'Schwarzenegger',      '1947-07-30'),
+('John',       'Travolta',            '1954-02-18'),
+('Samuel L.',  'Jackson',             '1948-12-21'),
+('Uma',        'Thurman',             '1970-04-29'),
+('Lucy',       'Liu',                 '1968-12-02'),
+('Brad',       'Pitt',                '1963-12-18'),
+('Christoph',  'Waltz',               '1956-10-04'),
+('Jamie',      'Foxx',                '1967-12-13'),
+('Kerry',      'Washington',          '1977-01-31'),
+('Margot',     'Robbie',              '1990-07-02'),
+('Timothy',    'Olyphant',            '1968-05-20'),
+('Harrison',   'Ford',                '1942-07-13'),
+('Rutger',     'Hauer',               '1944-01-23'),
+('Russell',    'Crowe',               '1964-04-07'),
+('Joaquin',    'Phoenix',             '1974-10-28'),
+('Matt',       'Damon',               '1970-10-08'),
+('Jessica',    'Chastain',            '1977-03-24'),
+('Josh',       'Hartnett',            '1978-07-21'),
+('Ewan',       'McGregor',            '1971-03-31'),
+('Tom',        'Skerritt',            '1933-08-25'),
+('Veronica',   'Cartwright',          '1949-04-20'),
+('Bee',        'Vang',                '1991-11-04'),
+('Ahney',      'Her',                 '1992-07-13'),
+('Hilary',     'Swank',               '1974-07-30'),
+('Morgan',     'Freeman',             '1937-06-01'),
+('Bradley',    'Cooper',              '1975-01-05'),
+('Sienna',     'Miller',              '1981-12-28'),
+('Sean',       'Penn',                '1960-08-17'),
+('Kevin',      'Bacon',               '1958-07-08'),
+('Gene',       'Hackman',             '1930-01-30'),
+('Richard',    'Harris',              '1930-10-01'),
+('Diane',      'Keaton',              '1946-01-05'),
+('Tony',       'Roberts',             '1939-10-22'),
+('Mariel',     'Hemingway',           '1961-11-22'),
+('Michael',    'Murphy',              '1938-05-05'),
+('Owen',       'Wilson',              '1968-11-18'),
+('Rachel',     'McAdams',             '1978-11-17'),
+('Jonathan', 'Meyers',            '1977-07-27'),
+('Scarlett',   'Johansson',           '1984-11-22'),
+('Penélope',   'Cruz',                '1974-04-28'),
+('Rebecca',    'Hall',                '1982-05-03');
 
--- Pulp Fiction
-('John Travolta', '1954-02-18'),
-('Samuel L. Jackson', '1948-12-21'),
-
--- Kill Bill: Vol. 1
-('Uma Thurman', '1970-04-29'),
-('Lucy Liu', '1968-12-02'),
-
--- Inglourious Basterds
-('Brad Pitt', '1963-12-18'),
-('Christoph Waltz', '1956-10-04'),
-
--- Django Unchained
-('Jamie Foxx', '1967-12-13'),
-('Kerry Washington', '1977-01-31'),
-
--- Once Upon a Time in Hollywood
-('Margot Robbie', '1990-07-02'),
-('Timothy Olyphant', '1968-05-20'),
-
--- Blade Runner
-('Harrison Ford', '1942-07-13'),
-('Rutger Hauer', '1944-01-23'),
-
--- Gladiator
-('Russell Crowe', '1964-04-07'),
-('Joaquin Phoenix', '1974-10-28'),
-
--- The Martian
-('Matt Damon', '1970-10-08'),
-('Jessica Chastain', '1977-03-24'),
-
--- Black Hawk Down
-('Josh Hartnett', '1978-07-21'),
-('Ewan McGregor', '1971-03-31'),
-
--- Alien
-('Tom Skerritt', '1933-08-25'),
-('Veronica Cartwright', '1949-04-20'),
-
--- Gran Torino
-('Bee Vang', '1991-11-04'),
-('Ahney Her', '1992-07-13'),
-
--- Million Dollar Baby
-('Hilary Swank', '1974-07-30'),
-('Morgan Freeman', '1937-06-01'),
-
--- American Sniper
-('Bradley Cooper', '1975-01-05'),
-('Sienna Miller', '1981-12-28'),
-
--- Mystic River
-('Sean Penn', '1960-08-17'),
-('Kevin Bacon', '1958-07-08'),
-
--- Unforgiven
-('Gene Hackman', '1930-01-30'),
-('Richard Harris', '1930-10-01'),
-
--- Annie Hall
-('Diane Keaton', '1946-01-05'),
-('Tony Roberts', '1939-10-22'),
-
--- Manhattan
-('Mariel Hemingway', '1961-11-22'),
-('Michael Murphy', '1938-05-05'),
-
--- Midnight in Paris
-('Owen Wilson', '1968-11-18'),
-('Rachel McAdams', '1978-11-17'),
-
--- Match Point
-('Jonathan Rhys Meyers', '1977-07-27'),
-('Scarlett Johansson', '1984-11-22'),
-
--- Vicky Cristina Barcelona
-('Penélope Cruz', '1974-04-28'),
-('Rebecca Hall', '1982-05-03');
---počet záznamů: 61
+-- 68 záznamů
 
 -- Insert Users
 INSERT INTO Users (Username, Email) VALUES 

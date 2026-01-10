60f984ef2ff4c3523c2f65059c1d8e0f726433d5
Matyáš Dvořák <otis24680@gmail.com>
2026-01-10 19:45:32 +0100

    ORM využití v Pythonu pomocí SQLAlchemy

---
+++ Dotazy, views a funkctions/Úkol 8. orm_test.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://admin:admin@localhost:5432/film_database"
+
+# Vytvoření motoru (engine) pro komunikaci s DB
+engine = create_engine(DATABASE_URL)
+# Základní třída pro ORM modely
+Base = declarative_base()
+
+
+class Film(Base):
+    __tablename__ = 'films'
+    
+    
+    film_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_year = Column(Integer)
+    rating = Column(Float)
+
+    
+    def __repr__(self):
+        return f"<Film(Název='{self.title}', Rok={self.release_year}, Hodnocení={self.rating})>"
+
+# 3. Vytvoření Session (relace pro komunikaci)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+print("--- Výpis všech Sci-Fi filmů (pomocí Pythonu) ---")
+sci_fi_movies = session.query(Film).filter(Film.rating > 8.0).limit(5).all()
+
+for movie in sci_fi_movies:
+    print(movie)
